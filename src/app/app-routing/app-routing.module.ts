22e30ba0277546343f81f7f300b14c4793a97fb7
hoang tung son <hoangtungson164@gmail.com>
2019-11-13 15:38:03 +0700

    fix routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ListCardComponent} from '../list-card/list-card.component';
 import {BoardComponent} from '../board/board.component';
+import {LoginComponent} from '../login/login.component';
+import {UserComponent} from '../user/user.component';
+import {RegisterComponent} from '../register/register.component';
 
 const routes: Routes = [{
   path: 'board/:id/list',
   component: BoardComponent
 }, {
+  path: 'login',
+  component: LoginComponent
+}, {
   path: 'list/:id',
   component: ListCardComponent
-}];
+}, {
+    path: 'user',
+    component: UserComponent
+  }, {
+    path: 'register',
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   declarations: [],
