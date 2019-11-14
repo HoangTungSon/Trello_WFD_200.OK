ec685062f8d1ba90e144d79195f414ad00b70a1b
hoang tung son <hoangtungson164@gmail.com>
2019-11-14 17:18:30 +0700

    adding list CRUD, add card, moving in one list

--- src/app/list-card/service/list-card.service.ts
+++ src/app/list-card/service/list-card.service.ts
@@ -28,8 +28,8 @@ export class ListCardService {
     return this.httpClient.post<IListCard>(this.URL, listCard);
   }
 
-  updateListCard(listCard: IListCard): Observable<IListCard> {
-    return this.httpClient.put<IListCard>(`${this.URL}/${listCard.listId}`, listCard);
+  updateListCard(listCard: IListCard, id: number): Observable<IListCard> {
+    return this.httpClient.put<IListCard>(`${this.URL}/${id}`, listCard);
   }
 
   deleteListCard(id: number): Observable<any> {
