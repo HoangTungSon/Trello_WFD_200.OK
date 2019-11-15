996cac6e074ed9dedc1d35b72a77738da8b7fdb5
ducninh1806@gmail.com <ducninh1806@gmail.com>
2019-11-15 11:28:59 +0700

    add signOut

--- src/app/login-taskbar/login-taskbar.component.ts
+++ src/app/login-taskbar/login-taskbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {TokenStorageService} from '../auth/token-storage.service';
+import {AuthService} from '../auth/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-taskbar',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginTaskbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(    private authService: AuthService,
+                  private tokenStorage: TokenStorageService,
+                  private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.tokenStorage.signOut();
+    this.router.navigate(['/']);
+    console.log(this.tokenStorage.getUsername());
   }
 
 }
